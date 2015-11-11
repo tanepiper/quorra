3efb80581c0cae5867fcaeafd654053ecba977d4
Tane Piper <piper.tane@gmail.com>
2015-11-11 12:44:41 +0000

    Check for fingerprint in path and remove

--- test/test.spec.js
+++ test/test.spec.js
@@ -13,12 +13,14 @@ lab.experiment('Hapi React Handler', () => {
   let server;
 
   lab.beforeEach((done) => {
+
     server = new Hapi.Server();
     server.connection({ port: 9000 });
 
     server.register([{
       register: require('./../')
     }], (error) => {
+
       if (error) {
         return done(error);
       }
@@ -39,19 +41,41 @@ lab.experiment('Hapi React Handler', () => {
   });
 
   lab.afterEach((done) => {
+
     server.stop(done);
   });
 
   lab.test('Set up handler on route', (done) => {
+
     const options = {
       method: 'GET',
       url: '/foo'
     };
 
     server.inject(options, (response) => {
+
       Code.expect(response.payload).match(/Hapi React Handler/);
       done();
     });
+  });
+
+  lab.test('decorates reply', (done) => {
+
+    server.inject('/foo', (response) => {
+
+      Code.expect(response.statusCode).to.equal(200);
+      Code.expect(response.request.response.variety).to.equal('react');
+      done();
+    });
+  });
+
+  lab.test('throws on server decoration root conflict', (done) => {
+
+    Code.expect(() => {
+
+      server.decorate('reply', 'react', () => { });
+    }).to.throw('Reply interface decoration already defined: react');
+    done();
   });
 });
 
@@ -61,12 +85,14 @@ lab.experiment('Hapi React Handler with layout', () => {
   let server;
 
   lab.beforeEach((done) => {
+
     server = new Hapi.Server();
     server.connection({ port: 9000 });
 
     server.register([{
       register: require('./../')
     }], (error) => {
+
       if (error) {
         return done(error);
       }
@@ -91,17 +117,176 @@ lab.experiment('Hapi React Handler with layout', () => {
   });
 
   lab.afterEach((done) => {
+
     server.stop(done);
   });
 
   lab.test('Set up handler on route', (done) => {
+
     const options = {
       method: 'GET',
       url: '/foo'
     };
 
     server.inject(options, (response) => {
+
       Code.expect(response.payload).match(/<title(.*?>Foobar)<\/title>/);
+      Code.expect(response.payload).match(/Hapi React Handler/);
+      done();
+    });
+  });
+});
+
+lab.experiment('Hapi React Handler specific path', () => {
+
+  let server;
+
+  lab.beforeEach((done) => {
+
+    server = new Hapi.Server();
+    server.connection({ port: 9000 });
+
+    server.register([{
+      register: require('./../')
+    }], (error) => {
+
+      if (error) {
+        return done(error);
+      }
+
+      server.route({
+        method: 'GET',
+        path: '/foo',
+        handler: {
+          react: {
+            relativeTo: Path.join(__dirname, 'assets'),
+            routerFile: 'router.jsx'
+          }
+        }
+      });
+
+      server.start(done);
+    });
+  });
+
+  lab.afterEach((done) => {
+
+    server.stop(done);
+  });
+
+  lab.test('Set up handler on route', (done) => {
+
+    const options = {
+      method: 'GET',
+      url: '/foo'
+    };
+
+    server.inject(options, (response) => {
+
+      Code.expect(response.payload).match(/Hapi React Handler/);
+      done();
+    });
+  });
+});
+
+lab.experiment('Hapi React Handler sub-path', () => {
+
+  let server;
+
+  lab.beforeEach((done) => {
+
+    server = new Hapi.Server();
+    server.connection({ port: 9000 });
+
+    server.register([{
+      register: require('./../')
+    }], (error) => {
+
+      if (error) {
+        return done(error);
+      }
+
+      server.route({
+        method: 'GET',
+        path: '/bar/{route*}',
+        handler: {
+          react: {
+            relativeTo: Path.join(__dirname, 'assets'),
+            routerFile: 'router.jsx'
+          }
+        }
+      });
+
+      server.start(done);
+    });
+  });
+
+  lab.afterEach((done) => {
+
+    server.stop(done);
+  });
+
+  lab.test('Set up handler on route', (done) => {
+
+    const options = {
+      method: 'GET',
+      url: '/bar/foo'
+    };
+
+    server.inject(options, (response) => {
+
+      Code.expect(response.payload).match(/Hapi React Handler/);
+      done();
+    });
+  });
+});
+
+lab.experiment('Hapi React Handler multiple sub-path', () => {
+
+  let server;
+
+  lab.beforeEach((done) => {
+
+    server = new Hapi.Server();
+    server.connection({ port: 9000 });
+
+    server.register([{
+      register: require('./../')
+    }], (error) => {
+
+      if (error) {
+        return done(error);
+      }
+
+      server.route({
+        method: 'GET',
+        path: '/bar/baz/{route*}',
+        handler: {
+          react: {
+            relativeTo: Path.join(__dirname, 'assets'),
+            routerFile: 'router.jsx'
+          }
+        }
+      });
+
+      server.start(done);
+    });
+  });
+
+  lab.afterEach((done) => {
+
+    server.stop(done);
+  });
+
+  lab.test('Set up handler on route', (done) => {
+
+    const options = {
+      method: 'GET',
+      url: '/bar/baz/foo'
+    };
+
+    server.inject(options, (response) => {
+
       Code.expect(response.payload).match(/Hapi React Handler/);
       done();
     });

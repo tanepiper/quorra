ec745c7833cf9995171e50829f96f444288deece
Tane Piper <piper.tane@gmail.com>
2015-11-16 10:53:15 +0000

    Create a noop function if window is not available

--- example/app/router.jsx
+++ example/app/router.jsx
@@ -4,7 +4,6 @@ const React = require('react');
 const Router = require('react-router').Router;
 const Route = require('react-router').Route;
 const IndexRoute = require('react-router').IndexRoute;
-const createHistory = require('history/lib').createHistory;
 
 const App = require('./app.jsx');
 const Hello = require('./hello.jsx');
@@ -12,31 +11,16 @@ const Goodbye = require('./goodbye.jsx');
 const Foo = require('./foo.jsx');
 const Links = require('./links.jsx');
 
+// We have to pass a noop for server-side as window is not available in node
+const createHistory = typeof window !== 'undefined' ? require('history/lib').createHistory : () => ({});
 
-// Note: This is a lazy implementation, cannot use createHistory on the
-// server side
-const isBrowser = typeof window !== 'undefined';
-
-if (isBrowser) {
-  module.exports = <Router history={createHistory()}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Links}/>
-      <Route path="hello" component={Hello} />
-      <Route path="goodbye" component={Goodbye} />
-    </Route>
-    <Route path="/foo" component={Foo}>
-      <IndexRoute component={Links}/>
-    </Route>
-  </Router>;
-} else {
-  module.exports = <Router>
-    <Route path="/" component={App}>
-      <IndexRoute component={Links}/>
-      <Route path="hello" component={Hello} />
-      <Route path="goodbye" component={Goodbye} />
-    </Route>
-    <Route path="/foo" component={Foo}>
-      <IndexRoute component={Links}/>
-    </Route>
-  </Router>;
-}
+module.exports = <Router history={createHistory()}>
+  <Route path="/" component={App}>
+    <IndexRoute component={Links}/>
+    <Route path="hello" component={Hello} />
+    <Route path="goodbye" component={Goodbye} />
+  </Route>
+  <Route path="/foo" component={Foo}>
+    <IndexRoute component={Links}/>
+  </Route>
+</Router>;
